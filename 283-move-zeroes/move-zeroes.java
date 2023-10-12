68e5c38d5f41c135040b663619bc47ba9169dd81
Naman Madharia <nbmadharia@gmail.com>
2023-10-12 08:18:26 +0530

    Time: 2 ms (68.41%) | Memory: 45.1 MB (34.89%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int nonZero=0;
+        for(int i=0; i<nums.length;i++){
+            if(nums[i] != 0 ){
+                nums[nonZero]=nums[i];
+                nonZero++;
+            }
+        }
+        
+        for(int i=nonZero ; i<nums.length ;i++){
+            nums[i]=0;
+        }
+        
+    }
+}
