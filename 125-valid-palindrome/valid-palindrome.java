24d8e67fbd7697e2c888e81e1177dc29ff6cc32e
Naman Madharia <nbmadharia@gmail.com>
2024-10-03 03:38:44 +0530

    Time: 242 ms (5.10%) | Memory: 46 MB (8.06%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.replaceAll("[^A-Za-z0-9]","");
+        s=s.toLowerCase();
+        // System.out.println(s);
+        
+        int i = 0;
+        int j = s.length()-1;
+        char first;
+        char second;
+        // System.out.println(second);
+        while(i<= j){
+            first = s.charAt(i);
+            second = s.charAt(j);
+            
+            System.out.println("i is: "+ i + " j is: "+ j);
+            System.out.println("first is: "+first);
+            System.out.println("second is: " + second);
+            
+            if(first != second){
+                
+                return false;
+            }
+            i++;j--;
+        }
+        return true;
+    }
+}
