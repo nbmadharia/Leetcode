5a6a089045abd0654307626136c376ac2112a4b8
Naman Madharia <nbmadharia@gmail.com>
2025-02-03 00:10:27 +0530

    Time: 1 ms (99.80%) | Memory: 53.4 MB (21.54%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,30 +1,17 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> mp = new HashMap<>();
-        int out=0;
-        for (int num : nums) {
-            if (mp.containsKey(num)) {
-                // System.out.println(mp);
+        int count =0;
+        int candidate =0;
 
-                mp.put(num, mp.get(num) + 1);
-            } else {
-                mp.put(num, 1);
+        for (int num : nums){
+            if (count == 0){
+                candidate = num;
             }
+            count = (candidate == num)? count+1 : count-1;
+            // if (candidate = num){
+            //     count++;
+            // }
         }
-        int maxValue = Collections.max(mp.values());
-        System.out.println("mp: "+ mp);
-        System.out.println("max value: " + maxValue);
-
-        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
-            System.out.println("after for: " + entry.getKey());
-
-            if (entry.getValue().equals(maxValue)) {
-                System.out.println("inside if max value: "+ maxValue);
-
-                out = entry.getKey();
-                System.out.println("maxValue if: " + maxValue);
-            }
-        }
-        return out;
+        return candidate;
     }
 }
