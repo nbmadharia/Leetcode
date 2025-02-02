13af3bd290e131932db416f53118a5bb44edc682
Naman Madharia <nbmadharia@gmail.com>
2025-02-03 00:01:31 +0530

    Time: 26 ms (7.45%) | Memory: 48.4 MB (99.77%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        int out=-20;
+        for (int num : nums) {
+            if (mp.containsKey(num)) {
+                // System.out.println(mp);
+
+                mp.put(num, mp.get(num) + 1);
+            } else {
+                mp.put(num, 1);
+            }
+        }
+        int maxValue = Collections.max(mp.values());
+        System.out.println("mp: "+ mp);
+        System.out.println("max value: " + maxValue);
+
+        for (Map.Entry<Integer, Integer> entry : mp.entrySet()) {
+            System.out.println("after for: " + entry.getKey());
+
+            if (entry.getValue().equals(maxValue)) {
+                System.out.println("inside if max value: "+ maxValue);
+
+                out = entry.getKey();
+                System.out.println("maxValue if: " + maxValue);
+            }
+        }
+        return out;
+    }
+}
