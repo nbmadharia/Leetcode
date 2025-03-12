9aeb0b573e591d331e84e1c347f68a8a5f30c987
Naman Madharia <nbmadharia@gmail.com>
2025-03-12 22:39:06 +0530

    Time: 1 ms (89.77%) | Memory: 45.3 MB (13.44%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,24 +1,24 @@
 class Solution {
     public int maxProduct(int[] nums) {
 
-        int[] max = new int[nums.length];
-        int[] min = new int[nums.length];
+        // int[] max = new int[nums.length];
+        // int[] min = new int[nums.length];
 
-        max[0] = min[0] = nums[0];
-        int result = nums[0];
-        for (int i = 1; i < nums.length; i++) {
+        // max[0] = min[0] = nums[0];
+        // int result = nums[0];
+        // for (int i = 1; i < nums.length; i++) {
 
-            if(nums[i]>0){
-                max[i] = Math.max(nums[i],max[i-1] * nums[i]);
-                min[i] = Math.min(nums[i],min[i-1] * nums[i]);
-            }
-            else{
-                max[i] = Math.max(nums[i],min[i-1] * nums[i]);
-                min[i] = Math.min(nums[i],max[i-1] * nums[i]);
-            }
-            result= Math.max(max[i],result);
-        }
-        return result;
+        //     if(nums[i]>0){
+        //         max[i] = Math.max(nums[i],max[i-1] * nums[i]);
+        //         min[i] = Math.min(nums[i],min[i-1] * nums[i]);
+        //     }
+        //     else{
+        //         max[i] = Math.max(nums[i],min[i-1] * nums[i]);
+        //         min[i] = Math.min(nums[i],max[i-1] * nums[i]);
+        //     }
+        //     result= Math.max(max[i],result);
+        // }
+        // return result;
 
         // int maxProduct = nums[0];
         // int minProduct = nums[0];
@@ -38,6 +38,23 @@ class Solution {
         // }
 
         // return result;
+           int n = nums.length;
+        int maxProduct = Integer.MIN_VALUE;
+
+        int leftProduct = 1, rightProduct = 1;
+
+        for (int i = 0; i < n; i++) {
+            leftProduct *= nums[i];
+            rightProduct *= nums[n - 1 - i];
+
+            maxProduct = Math.max(maxProduct, Math.max(leftProduct, rightProduct));
+
+            if (leftProduct == 0) leftProduct = 1;
+            if (rightProduct == 0) rightProduct = 1;
+        }
+
+        return maxProduct;
+    
 
     }
 }
