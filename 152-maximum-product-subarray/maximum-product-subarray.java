04f79e9978eb55c3b7ebeec9d2e8b1862127d1d8
Naman Madharia <nbmadharia@gmail.com>
2025-03-12 22:36:27 +0530

    Time: 2 ms (57.03%) | Memory: 45.3 MB (13.44%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,43 +1,43 @@
 class Solution {
     public int maxProduct(int[] nums) {
 
-        // int[] max = new int[nums.length];
-        // int[] min = new int[nums.length];
+        int[] max = new int[nums.length];
+        int[] min = new int[nums.length];
 
-        // max[0] = min[0] = nums[0];
-        // int result = nums[0];
-        // for (int i = 1; i < nums.length; i++) {
-
-        //     if(nums[i]>0){
-        //         max[i] = Math.max(nums[i],max[i-1] * nums[i]);
-        //         min[i] = Math.min(nums[i],min[i-1] * nums[i]);
-        //     }
-        //     else{
-        //         max[i] = Math.max(nums[i],min[i-1] * nums[i]);
-        //         min[i] = Math.min(nums[i],max[i-1] * nums[i]);
-        //     }
-        //     result= Math.max(max[i],result);
-        // }
-        // return result;
-
-        int maxProduct = nums[0];
-        int minProduct = nums[0];
+        max[0] = min[0] = nums[0];
         int result = nums[0];
-
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] < 0) {
-                int temp = maxProduct;
-                maxProduct = minProduct;
-                minProduct = temp;
+
+            if(nums[i]>0){
+                max[i] = Math.max(nums[i],max[i-1] * nums[i]);
+                min[i] = Math.min(nums[i],min[i-1] * nums[i]);
             }
-
-            maxProduct = Math.max(nums[i], maxProduct * nums[i]);
-            minProduct = Math.min(nums[i], minProduct * nums[i]);
-
-            result = Math.max(result, maxProduct);
+            else{
+                max[i] = Math.max(nums[i],min[i-1] * nums[i]);
+                min[i] = Math.min(nums[i],max[i-1] * nums[i]);
+            }
+            result= Math.max(max[i],result);
         }
-
         return result;
+
+        // int maxProduct = nums[0];
+        // int minProduct = nums[0];
+        // int result = nums[0];
+
+        // for (int i = 1; i < nums.length; i++) {
+        //     if (nums[i] < 0) {
+        //         int temp = maxProduct;
+        //         maxProduct = minProduct;
+        //         minProduct = temp;
+        //     }
+
+        //     maxProduct = Math.max(nums[i], maxProduct * nums[i]);
+        //     minProduct = Math.min(nums[i], minProduct * nums[i]);
+
+        //     result = Math.max(result, maxProduct);
+        // }
+
+        // return result;
 
     }
 }
