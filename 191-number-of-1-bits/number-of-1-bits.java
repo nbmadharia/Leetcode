f07f0766c1c3878208c79c1d0d3dfcd755defa9e
Naman Madharia <nbmadharia@gmail.com>
2025-03-17 01:20:17 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (72.39%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.java
+++ 191-number-of-1-bits/number-of-1-bits.java
@@ -1,32 +1,32 @@
 class Solution {
     public int hammingWeight(int n) {
-        // int out = 0;
-        // if(n==1){
-        //     return 1;
-        // }
-        // // System.out.println("Outside while: " + n / 2);
-        // while (n / 2 >= 2) {
+        int out = 0;
+        if(n==1){
+            return 1;
+        }
+        // System.out.println("Outside while: " + n / 2);
+        while (n / 2 >= 2) {
             
-        //     // System.out.println("Out 1: " + out + " n: " + n);
-        //     if (n % 2 == 1) {
-        //         // System.out.println("Out 2 if: " + out + " n: " + n);
-        //         out++;
-        //     }
-        //     n /= 2;
-        // }
-        // if(n==2){
-        //     out++;
-        // }
-        // else if(n==3){
-        //     out+=2;
-        // }
-        int count=0;
-        while( n !=0){
-            count+= n & 1;
-            n=n>>1;
+            // System.out.println("Out 1: " + out + " n: " + n);
+            if (n % 2 == 1) {
+                // System.out.println("Out 2 if: " + out + " n: " + n);
+                out++;
+            }
+            n /= 2;
+        }
+        if(n==2){
+            out++;
+        }
+        else if(n==3){
+            out+=2;
+        }
+        // int count=0;
+        // while( n !=0){
+        //     count+= n & 1;
+        //     n=n>>1;
             // System.out.println("Outside while: " + count);
 
-        }
-        return count;
+        // }
+        return out;
     }
 }
